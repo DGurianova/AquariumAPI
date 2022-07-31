1b3bacfd1cd8bfd83e9e756c1b45fae74c724a7b
Danylo Gurianov <dgurianov@gmail.com>
2022-07-31 23:57:51 +0200

    Added logic to insert AND to query

--- src/main/java/com/gurianova/aquariumapi/controller/FishController.java
+++ src/main/java/com/gurianova/aquariumapi/controller/FishController.java
@@ -36,15 +36,21 @@ public class FishController {
 
     @PostMapping(value = "/fishes/search", produces = "application/json")
     public List<ResponseFishDTO> searchFish(@RequestBody RequestFishDTO request) {
+        //TODO Add exception handling  if no parameters provided  ,this will equal to find ALL fishes, which is served by other method in controller
         String baseQuery = "SELECT f FROM Fish f WHERE";
+        boolean queryParameterAlreadyExists = false;
         if (request.getName() != null) {
             baseQuery = baseQuery + "  fish_name  = '" + request.getName() + "'";
+            queryParameterAlreadyExists = true;
         }
         if (request.getAgeYears() != null) {
-            baseQuery = baseQuery + " and fish_age_years  = " + request.getAgeYears() + "";
+            if (queryParameterAlreadyExists){ baseQuery = baseQuery + " and "; } //If queryParameterAlreadyExists is  true , getName  was not null and was already added to query , so we need to add additional AND before add next parameter
+            baseQuery = baseQuery + " fish_age_years  = " + request.getAgeYears() + "";
+            queryParameterAlreadyExists = true;                                  //Because we just added new param with AND , we need to set this flag to true , to signal the next if to add AND
         }
         if (request.getPreferredFood() != null) {
-            baseQuery = baseQuery + " and preferred_food  = '" + request.getPreferredFood() + "'";
+            if (queryParameterAlreadyExists){baseQuery = baseQuery + " and ";}//If queryParameterAlreadyExists is true , getAgeYears() was not null and was already added to query , so we need to add additional AND before add next parameter
+            baseQuery = baseQuery + " preferred_food  = '" + request.getPreferredFood() + "'";
         }
         TypedQuery<Fish> query = manager.createQuery(baseQuery, Fish.class);
         return query.getResultList()
